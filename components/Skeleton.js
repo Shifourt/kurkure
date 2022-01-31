95e0f320c6d23b91dc52320d3179ce209a56c28f
Mahabub Hossain <sjl.hsn@gmail.com>
2022-01-31 22:11:25 +0600

    fallback

--- components/Skeleton.js
+++ components/Skeleton.js
@@ -1,33 +1,34 @@
 export default function skeleton() {
-  return 
-  <div className="skeleton">
-      <div className="s-banner"></div>
-      <div className="s-header"></div>
-      <div className="s-content"></div>
-      <div className="s-content"></div>
-      <div className="s-content"></div>
+  return (
+        <div className="skeleton">
+        <div className="s-banner"></div>
+        <div className="s-header"></div>
+        <div className="s-content"></div>
+        <div className="s-content"></div>
+        <div className="s-content"></div>
 
-      <style jsx>{`
+        <style jsx>{`
         .skeleton {
-          max-width: 1200px;
-          margin: 20px auto;
+            max-width: 1200px;
+            margin: 20px auto;
         }
         .skeleton > div {
-          background: #dbcc1a;
-          border-radius: 4px;
-          margin: 20px 0;
+            background: #dbcc1a;
+            border-radius: 4px;
+            margin: 20px 0;
         }
         .s-banner {
-          padding: 12% 0;
+            padding: 12% 0;
         }
         .s-header {
-          padding: 15px 0;
-          max-width: 500px;
+            padding: 15px 0;
+            max-width: 500px;
         }
         .s-content {
-          padding: 8px 0;
-          max-width: 1000px;
+            padding: 8px 0;
+            max-width: 1000px;
         }
-      `}</style>
-  </div>;
+        `}</style>
+    </div>;
+  )
 }
