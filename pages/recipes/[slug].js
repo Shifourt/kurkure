69640e19c6d60bf1fdc2fada03bee835d39c378b
Mahabub Hossain <sjl.hsn@gmail.com>
2022-01-31 20:58:57 +0600

    isr

--- pages/recipes/[slug].js
+++ pages/recipes/[slug].js
@@ -31,7 +31,10 @@ export async function getStaticProps({ params }) {
   })
 
   return {
-    props: { recipe: items[0] }
+    props: { 
+      recipe: items[0]
+    },
+    revalidate: 10
   }
 
 }
